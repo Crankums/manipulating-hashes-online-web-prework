80787d4cbb8199f71f80c70ab3f2fb352debde84
Craig Donavin <crdonavin@gmail.com>
2019-04-07 20:49:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      delete_if {|key, value| value == "strawberry"}
+      attribute.delete_if {|key, value| value == "strawberry"}
     end
   end
 
