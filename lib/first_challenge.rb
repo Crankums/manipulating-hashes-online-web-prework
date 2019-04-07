ecf88531329b068df6b116e89b35d6e66fdb82d5
Craig Donavin <crdonavin@gmail.com>
2019-04-07 20:57:23 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
 if contacts.any? { |attribute, data| data == "strawberry" }
   data.delete("strawberry")
-end 
+end
 
   #remember to return your newly altered contacts hash!
   contacts
